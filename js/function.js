7b89dc7b0a3603b93adbbd1b263f2b412634f00d
davit gelashvili <datoogelashvili@gmail.com>
2019-09-24 13:05:55 +0400

    add file design

---
+++ js/function.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $('.login-form-tab-login').click(function () {
+        $(this).addClass('active');
+        $('.login-form-item').addClass('active');
+        $('.reg-form-item').removeClass('active');
+        $('.login-form-tab-reg').removeClass('active');
+    });
+    $('.login-form-tab-reg').click(function () {
+        $(this).addClass('active');
+        $('.login-form-item').removeClass('active');
+        $('.reg-form-item').addClass('active');
+        $('.login-form-tab-login').removeClass('active');
+    })
+
+
+    $('.left-top-head > .first').click(function () {
+        $('.left-top-body.first').addClass('active');
+        $('.left-top-body.second').removeClass('active');
+        $('.left-top-body.third').removeClass('active');
+    })
+    $('.left-top-head > .second').click(function () {
+        $('.left-top-body.second').addClass('active');
+        $('.left-top-body.first').removeClass('active');
+        $('.left-top-body.third').removeClass('active');
+    })
+    $('.left-top-head > .third').click(function () {
+        $('.left-top-body.third').addClass('active');
+        $('.left-top-body.first').removeClass('active');
+        $('.left-top-body.second').removeClass('active');
+    })
+
+
+});
